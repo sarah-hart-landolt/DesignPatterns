7a74c0b3916c61a0b292b2b38c5d9962f34a0082
Sarah Hart Landolt <sarah.hart.landolt@gmail.com>
2020-10-12 11:21:04 -0500

    interface segregation finished

---
+++ SOLID/InterfaceSegregation/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace InterfaceSegregation
+{
+    public class Document
+    {
+    }
+
+    // the IMachine violates the interface segregation 
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Fax(Document d);
+        void Scan(Document d);
+    }
+
+    // above works if you need a multifunction machine, but not if something is oldfashioned
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    // example of how the non-segregated interfaces causes problems 
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Print(Document d)
+        {
+            // yep
+        }
+
+        public void Fax(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    // segragating the interfaces 
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Printer : IPrinter
+    {
+        public void Print(Document d)
+        {
+
+        }
+    }
+
+    // implementing 2 of the interfaces 
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    ///  creating an even higher level interface, implmenting IPrinter and IScanner
+    /// </summary>
+    public interface IMultiFunctionDevice : IPrinter, IScanner //
+    {
+
+    }
+
+    // a struct for constructing what a multifunction machine is using the interface 
+    public struct MultiFunctionMachine : IMultiFunctionDevice
+    {
+        // compose this out of several modules
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            if (printer == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(printer));
+            }
+            if (scanner == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(scanner));
+            }
+            this.printer = printer;
+            this.scanner = scanner;
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        }
+    }
+}
